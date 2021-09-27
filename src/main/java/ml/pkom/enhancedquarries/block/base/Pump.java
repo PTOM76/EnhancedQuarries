43fc089cde21055a040b06ff1b211f494df53663
PTOM76 <ptms76@outlook.jp>
2021-09-27 15:57:46 +0900

    EnhancedQuarry 1.2.0-RC4

--- src/main/java/ml/pkom/enhancedquarries/block/base/Pump.java
+++ src/main/java/ml/pkom/enhancedquarries/block/base/Pump.java
@@ -3,13 +3,17 @@ package ml.pkom.enhancedquarries.block.base;
 import alexiil.mc.lib.attributes.AttributeList;
 import alexiil.mc.lib.attributes.AttributeProvider;
 import alexiil.mc.lib.attributes.fluid.impl.EmptyFluidExtractable;
+import alexiil.mc.mod.pipes.blocks.TilePump;
 import ml.pkom.enhancedquarries.event.TileCreateEvent;
+import ml.pkom.enhancedquarries.tile.base.PumpTile;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
@@ -46,5 +50,7 @@ public abstract class Pump extends BaseBlock implements BlockEntityProvider, Att
 
     public abstract BlockEntity createBlockEntity(TileCreateEvent event);
 
-
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        return world.isClient ? null : (w, p, s, tile) -> ((PumpTile) tile).tick(w, p, s, (PumpTile) tile);
+    }
 }
